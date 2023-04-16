a69b3af6e31b23bbb7dc24da39731f506e5b0bf2
ziruihao <ziray.hao@gmail.com>
2023-04-16 11:03:20 -0400

    Reorganize demo data

--- demo-data/test.py
+++ demo-data/test.py
@@ -3,20 +3,18 @@ import requests
 import csv
 import datetime
 import random
+import time
 from dotenv import load_dotenv
 
 # Load the environment variables from the .env file
 load_dotenv()
 
 # Set up the OpenAI API credentials
-openai_api_key = os.getenv('OPENAI_SEC_KEY')
-openai_api_endpoint = 'https://api.openai.com/v1/completions'
+openai_api_endpoint = 'http://localhost:3000/read'
 
 # Set up the folder path for the .txt files
-folder_path = './demo-data/short'
+folder_path = './biology'
 
-# Set up the output CSV file path
-output_file_path = './prompting/test-output.csv'
 
 # Initialize an incremental ID counter
 id_counter = 0
@@ -30,26 +28,17 @@ for file_name in os.listdir(folder_path):
         # Read in the contents of the file
         with open(os.path.join(folder_path, file_name), 'r') as f:
             file_content = f.read()
-
-        prompt = '{}\n\n{}'.format('Today is {}. The user just read the article below. Generate list of keywords and metadata (such as author, time when published, current time, topics) around 256 characters in length.'.format(random_date_str = datetime.datetime.now() - datetime.timedelta(days=random.randint(0, 730)).date().isoformat()), file_content)
-
         # Make a POST request to the OpenAI API
-        headers = {'Authorization': f'Bearer {openai_api_key}', 'Content-Type': 'application/json',}
+        headers = {'Content-Type': 'application/json',}
         data = {
-            'model': 'text-davinci-003',
-            'prompt': prompt,
-            'max_tokens': 256
+            'blob': file_content
         }
-        response = requests.post(openai_api_endpoint, headers=headers, json=data)
-        print(response)
+        try:
+            response = requests.post(openai_api_endpoint, headers=headers, json=data).json()
+        except:
+            print('Error: ', file_name)
+            continue
+        print('date', response['timestamp'])
+        print('essence', response['metadata'])
 
-        # Extract the result string from the response
-        result_string = response.json()['choices'][0]['text']
-
-        results.append((file_name, result_string))
-
-# Write the results to a CSV file
-with open(output_file_path, 'w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['id', 'metadata'])
-    writer.writerows(results)
+        time.sleep(5)
