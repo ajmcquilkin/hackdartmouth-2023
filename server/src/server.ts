e9d5af52ecb923a859e8d2a9862936ff0b41f501
ziruihao <ziray.hao@gmail.com>
2023-04-16 11:04:47 -0400

    Updates

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import axios from 'axios';
-import mysql from 'mysql';
-import { handleQuestion } from "./prompt-engine";
+import { handleQuestion, generateMetadata } from "./prompt-engine";
+import { naturalDateFormat, generateRandomDate, toSQLiteDateTime } from "./lib";
+import { db } from "./db";
 
 const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,20 +14,22 @@ app.get("/", (req, res) => {
     res.send("You are listening from express server");
 });
 
-app.get("/question", async (req, res) => {
-    const connection = mysql.createConnection({
-        host: "localhost",
-        user: "hack",
-        password: "hack",
-        database: "hack"
-    });
-
-    connection.connect();
-
-    handleQuestion('SAMPLE Q');
-
-    connection.end();
+app.post("/question", async (req, res) => {
+    const { question, context } = req.body
+    const response = await handleQuestion(question, context);
+    res.send(response);
 });
+
+app.post('/read', async (req, res) => {
+    const { blob } = req.body;
+    const randomDay = generateRandomDate();
+    const metadata = await generateMetadata(blob, naturalDateFormat(randomDay));
+    db.prepare('INSERT INTO documents (timestamp, metadata, blob) VALUES (?, ?, ?)').run(toSQLiteDateTime(randomDay), `Date read: ${naturalDateFormat(randomDay)}. ${metadata}`, blob);
+    res.json({
+        timestamp: randomDay,
+        metadata,
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}`);
